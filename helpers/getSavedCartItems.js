a4a437f146de955666b08c250500097e3eeeb7c2
VictorSilva27 <victoradaosilva2@gmail.com>
2022-09-30 10:05:17 -0300

    Arrumando arquivos e pasta

--- helpers/getSavedCartItems.js
+++
@@ -1,9 +0,0 @@
-const getSavedCartItems = () => {
-  // seu código aqui
-  const get = localStorage.getItem('cartItems');
-  return get;
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = getSavedCartItems;
-}
--- helpers/saveCartItems.js
+++
@@ -1,9 +0,0 @@
-const saveCartItems = (setItem) => {
-  // seu código aqui
-  const save = localStorage.setItem('cartItems', setItem);
-  return save;
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = saveCartItems;
-}
